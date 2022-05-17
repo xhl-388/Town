a58779aa1398668d8d8756c63fc59e4cca3c57b0
XHL <2682950883@qq.com>
2022-05-17 22:03:34 +0800

    clean up the gun code

--- Assets/Easy FPS/Scripts/BulletScript.cs
+++ Assets/Easy FPS/Scripts/BulletScript.cs
@@ -1,11 +1,11 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 
 public class BulletScript : MonoBehaviour {
 
 	[Tooltip("Furthest distance bullet will look for target")]
 	public float maxDistance = 1000000;
-	RaycastHit hit;
 	[Tooltip("Prefab of wall damange hit. The object needs 'LevelPart' tag to create decal on it.")]
 	public GameObject decalHitWall;
 	[Tooltip("Decal will need to be sligtly infront of the wall so it doesnt cause rendeing problems so for best feel put from 0.01-0.1.")]
@@ -13,7 +13,14 @@ public class BulletScript : MonoBehaviour {
 	[Tooltip("Blood prefab particle this bullet will create upoon hitting enemy")]
 	public GameObject bloodEffect;
 	[Tooltip("Put Weapon layer and Player layer to ignore bullet raycast.")]
-	public LayerMask ignoreLayer;
+	private LayerMask _ignoreLayer;
+
+	private void Awake()
+	{
+		_ignoreLayer = 1 << LayerMask.NameToLayer("Player");
+	}
+
+	RaycastHit _hitInfo;
 
 	/*
 	* Uppon bullet creation with this script attatched,
@@ -22,14 +29,14 @@ public class BulletScript : MonoBehaviour {
 	*/
 	void Update () {
 
-		if(Physics.Raycast(transform.position, transform.forward,out hit, maxDistance, ~ignoreLayer)){
+		if(Physics.Raycast(transform.position, transform.forward,out _hitInfo, maxDistance, ~_ignoreLayer)){
 			if(decalHitWall){
-				if(hit.transform.tag == "LevelPart"){
-					Instantiate(decalHitWall, hit.point + hit.normal * floatInfrontOfWall, Quaternion.LookRotation(hit.normal));
+				if(_hitInfo.transform.CompareTag("Untagged")){
+					Instantiate(decalHitWall, _hitInfo.point + _hitInfo.normal * floatInfrontOfWall, Quaternion.LookRotation(_hitInfo.normal));
 					Destroy(gameObject);
 				}
-				if(hit.transform.tag == "Dummie"){
-					Instantiate(bloodEffect, hit.point, Quaternion.LookRotation(hit.normal));
+				if(_hitInfo.transform.CompareTag("Dummie")){
+					Instantiate(bloodEffect, _hitInfo.point, Quaternion.LookRotation(_hitInfo.normal));
 					Destroy(gameObject);
 				}
 			}		
--- Assets/Easy FPS/Scripts/DestroyAfterTimeParticle.cs
+++ Assets/Easy FPS/Scripts/DestroyAfterTimeParticle.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 public class DestroyAfterTimeParticle : MonoBehaviour {
 	[Tooltip("Time to destroy")]
-	public float timeToDestroy = 0.8f;
+	[SerializeField]
+	private float timeToDestroy = 0.8f;
 	/*
 	* Destroys gameobject after its created on scene.
 	* This is used for particles and flashes.
